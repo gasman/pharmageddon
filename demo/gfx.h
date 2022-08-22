45300b1b0a3d5abc6b7a248d396405cde55b18cd
Matt Westcott <matt@west.co.tt>
2022-08-22 20:28:41 +0100

    circle

--- demo/gfx.h
+++ demo/gfx.h
@@ -5,3 +5,4 @@ void gfx_putpixel(uint32_t *pixels, int x, int y, uint32_t colour);
 void gfx_rect(uint32_t *pixels, int x, int y, int w, int h, uint32_t colour);
 void gfx_fillrect(uint32_t *pixels, int x, int y, int w, int h, uint32_t colour);
 void gfx_line(uint32_t *pixels, int x1, int y1, int x2, int y2, uint32_t colour);
+void gfx_circle(uint32_t *pixels, int cx, int cy, int r, uint32_t colour);
