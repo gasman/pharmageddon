f4104952b7487644733cbcb662804512ef94f6ac
Matt Westcott <matt@west.co.tt>
2022-08-22 02:13:02 +0100

    use a static library for the demo code

--- demo/demo.c
+++ demo/demo.c
@@ -1,19 +1,9 @@
-#include <math.h>
-#include "demo.h"
+#include "plasma.h"
 
 void demo_init(void) {
+    plasma_init();
 }
 
 void demo_frame(uint32_t *pixels, uint32_t time) {
-    double dt = ((double)time) / 499;
-
-    for (int y = 0; y < 192; y++) {
-        for (int x = 0; x < 192; x++) {
-            double dx = (double)x;
-            double dy = (double)y;
-            double v = sin(dx/50+dt)+sin(dy/22+dt)+sin(dx/16);
-
-            pixels[x + y * 192] = (((int)(128 + v * 64)) & 255) << 24;
-        }
-    }
+    plasma_frame(pixels, time);
 }
---
+++ demo/plasma.h
@@ -0,0 +1,4 @@
+#include <stdint.h>
+
+void plasma_init(void);
+void plasma_frame(uint32_t *pixels, uint32_t time);
