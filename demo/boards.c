17f09e3ddc4cbb59e67d5b2fe5ba7118ff28ad8f
superogue <superogue1@protonmail.com>
2022-09-22 22:51:30 +0200

    effects - updated boards effect

--- demo/boards.c
+++ demo/boards.c
@@ -11,15 +11,15 @@ void boards_frame(uint32_t *pixels, uint32_t time)
 int t=time/20;
 int part=t>>8;
 int tt=t;
-float zoom=sin(t/24)*4+5;
+float zoom=sin(t/24)*2+3;
 for (int y=0;y<192;y++) {
     for (int x=0;x<192;x++) {
         int sx=abs(x-96);
         int sy=abs(y-96);
         int q=0;
         for (int l=1;l<5;l++) {
-            int qn=(int)(sx*zoom-tt) & (int)(sy * zoom-tt);
-            if (part>1) qn=(int)(sx*zoom-tt) ^ (int)(sy * zoom-tt);
+            int qn=(int)(abs(sx+sy)*zoom-tt) & (int)(abs(sx-sy) * zoom-tt);
+          //  if (part>1) qn=(int)((sx+sy)*zoom-tt) ^ (int)((sx-sy) * zoom-tt);
 
             q=q|qn;
         }
