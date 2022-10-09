a0c6c1d36df2575bc3a6f02811e1b876b37a170a
Matt Westcott <matt@west.co.tt>
2022-10-09 17:22:30 +0100

    Move back-face test into gfx3d_flat_tri
    (might have flipped the sign, but I don't think I checked that was correct in the first place)

--- demo/gfx3d.c
+++ demo/gfx3d.c
@@ -2,6 +2,11 @@
 #include "gfx.h"
 
 void gfx3d_flat_tri(uint32_t *pixels, double *zbuffer, vec3 v1, vec3 v2, vec3 v3, uint32_t colour) {
+    /* find z component of cross product to test for back-face culling */
+    double xpz = (v2.x - v1.x) * (v3.y - v1.y) - (v2.y - v1.y) * (v3.x - v1.x);
+
+    if (xpz > 0) return;
+
     gfx_filltri(
         pixels,
         (int)(96 + 96 * v1.x),
