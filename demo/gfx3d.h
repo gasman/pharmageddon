4230db166c894cbc713145c201dbe11624d22ce6
Matt Westcott <matt@west.co.tt>
2022-10-11 00:56:18 +0100

    Use matrices for transformation. (TODO: figure out how to get perspective working...)

--- demo/gfx3d.h
+++ demo/gfx3d.h
@@ -6,4 +6,16 @@ typedef struct vec3 {
     double z;
 } vec3;
 
+typedef double mat4[16];
+typedef double mat3[9];
+
+void mat4_identity(mat4 out);
+void mat4_ortho(mat4 out, double left, double right, double bottom, double top, double znear, double zfar);
+void mat4_mul(mat4 out, mat4 m1, mat4 m2);
+vec3 mat4_mul_vec3(vec3 vec, mat4 mat);
+void mat4_rotate_x(mat4 mat, double angle);
+void mat4_rotate_y(mat4 mat, double angle);
+void mat4_translate(mat4 mat, vec3 vec);
+void mat4_to_inverse_mat3(mat4 mat, mat3 dest);
+
 void gfx3d_flat_tri(uint32_t *pixels, double *zbuffer, vec3 v1, vec3 v2, vec3 v3, uint32_t colour);
--- demo/teapot.c
+++ demo/teapot.c
@@ -9,7 +9,7 @@
 #include "gfx3d.h"
 #include "teapot.h"
 
-struct aiVector3D *transformed_vertices;
+vec3 *model_vertices, *transformed_vertices;
 const struct aiMesh *teapot;
 unsigned int vertex_count;
 unsigned int face_count;
@@ -31,7 +31,19 @@ void teapot_init(void) {
         vertex_count = teapot->mNumVertices;
         face_count = teapot->mNumFaces;
 
-        transformed_vertices = malloc(vertex_count * sizeof(struct aiVector3D));
+        model_vertices = malloc(vertex_count * sizeof(vec3));
+        if (model_vertices == NULL) {
+            printf("could not allocate model_vertices\n");
+        }
+
+        for (unsigned int i = 0; i < vertex_count; i++) {
+            struct aiVector3D v = teapot->mVertices[i];
+            model_vertices[i].x = v.x;
+            model_vertices[i].y = v.y;
+            model_vertices[i].z = v.z;
+        }
+
+        transformed_vertices = malloc(vertex_count * sizeof(vec3));
         if (transformed_vertices == NULL) {
             printf("could not allocate transformed_vertices\n");
         }
@@ -41,16 +53,21 @@ void teapot_init(void) {
 }
 
 void teapot_frame(uint32_t *pixels, uint32_t time) {
+    mat4 rotate_matrix;
+    // mat4 projection_matrix;
+
     gfx_cls(pixels, 0x00110000);
 
-    double r = ((double)time) / 1000;
+    mat4_identity(rotate_matrix);
+    vec3 trans1 = {0, -1, 1};
+    mat4_translate(rotate_matrix, trans1);
+    mat4_rotate_y(rotate_matrix, ((double)time) / 1000);
+    mat4_rotate_x(rotate_matrix, ((double)time) / 900);
+    // mat4_ortho( projection_matrix, -5.0, 5.0, 5.0, -5.0, -30.0, 100.0 );
 
     for (unsigned int i = 0; i < vertex_count; i++) {
-        struct aiVector3D v = teapot->mVertices[i];
-
-        transformed_vertices[i].x = v.x * cos(r) + v.z * sin(r);
-        transformed_vertices[i].y = v.y;
-        transformed_vertices[i].z = v.z * cos(r) - v.x * sin(r);
+        vec3 pos = mat4_mul_vec3(model_vertices[i], rotate_matrix);
+        transformed_vertices[i] = pos; // mat4_mul_vec3(pos, projection_matrix);
     }
 
     for (double *zbuffer_ptr = zbuffer; zbuffer_ptr < (zbuffer + 192*192); zbuffer_ptr++) {
@@ -59,13 +76,13 @@ void teapot_frame(uint32_t *pixels, uint32_t time) {
 
     for (unsigned int i = 0; i < face_count; i++) {
         struct aiFace face = teapot->mFaces[i];
-        struct aiVector3D v0 = transformed_vertices[face.mIndices[0]];
-        struct aiVector3D v1 = transformed_vertices[face.mIndices[1]];
-        struct aiVector3D v2 = transformed_vertices[face.mIndices[2]];
+        vec3 v0 = transformed_vertices[face.mIndices[0]];
+        vec3 v1 = transformed_vertices[face.mIndices[1]];
+        vec3 v2 = transformed_vertices[face.mIndices[2]];
 
-        vec3 sv1 = {v0.x / 5, v0.y / 5 - 0.3, v0.z};
-        vec3 sv2 = {v1.x / 5, v1.y / 5 - 0.3, v1.z};
-        vec3 sv3 = {v2.x / 5, v2.y / 5 - 0.3, v2.z};
+        vec3 sv1 = {v0.x/5, v0.y/5, v0.z};
+        vec3 sv2 = {v1.x/5, v1.y/5, v1.z};
+        vec3 sv3 = {v2.x/5, v2.y/5, v2.z};
         gfx3d_flat_tri(
             pixels, zbuffer, sv1, sv2, sv3,
             (128 + (i & 127)) * 0x01010100
