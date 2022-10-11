028679dc8032fafb531da997634ce2dec05a8481
Matt Westcott <matt@west.co.tt>
2022-10-11 02:37:43 +0100

    remember to transform the normals too

--- demo/gfx3d.h
+++ demo/gfx3d.h
@@ -13,6 +13,7 @@ void mat4_identity(mat4 out);
 void mat4_ortho(mat4 out, double left, double right, double bottom, double top, double znear, double zfar);
 void mat4_mul(mat4 out, mat4 m1, mat4 m2);
 vec3 mat4_mul_vec3(vec3 vec, mat4 mat);
+vec3 mat3_mul_vec3(vec3 vec, mat3 mat);
 void mat4_rotate_x(mat4 mat, double angle);
 void mat4_rotate_y(mat4 mat, double angle);
 void mat4_translate(mat4 mat, vec3 vec);
